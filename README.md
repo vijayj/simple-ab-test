c442e5ae407852a77cecd84df514773184e7ae59
Vijay Jambu <vjambu@us-vjambu.local>
2016-02-23 16:52:50 -0800

    initial app commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# optimizely-ab-test
+an app to demonstrate ab test
+
+First screen just a simple view
+
+A page view controller that changes pages based on input email or not - A/B test
+
+Taking them to same tab or different tab based on A/B test
+
---
+++ todos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  todos
+//
+//  Created by Vijay Jambu on 2/23/16.
+//  Copyright © 2016 Vijay Jambu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
